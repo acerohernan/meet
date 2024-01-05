76fc2c072e4a664d335a5501de915876701f5f8c
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-04 21:12:14 -0500

    feat(web): new meeting page added

--- apps/web/src/pages/room/meeting.tsx
+++ apps/web/src/pages/room/meeting.tsx
@@ -1,7 +1,49 @@
-import { useParams } from "react-router-dom";
+import { Box, IconButton } from "@mui/material";
 
+import MicIcon from "@mui/icons-material/Mic";
+import MicOffIcon from "@mui/icons-material/MicOff";
+import VideocamOffOutlinedIcon from "@mui/icons-material/VideocamOffOutlined";
+import VideocamOutlinedIcon from "@mui/icons-material/VideocamOutlined";
+
+import { useState } from "react";
 export const MeetingPage = () => {
-  const { roomId } = useParams();
+  const [micOn, setMicOn] = useState(false);
+  const [cameraOn, setCameraOn] = useState(false);
 
-  return <div>MeetingPage: {roomId} </div>;
+  function toggleMicrophone() {
+    setMicOn((prev) => !prev);
+  }
+
+  function toggleCamera() {
+    setCameraOn((prev) => !prev);
+  }
+
+  return (
+    <Box
+      width="100%"
+      height="100vh"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#202124",
+        padding: 4,
+      }}
+    >
+      <Box sx={{ flex: 1 }}></Box>
+      <Box width="100%" sx={{ display: "flex", gap: 2 }}>
+        <IconButton
+          color={micOn ? "active" : "inactive"}
+          onClick={toggleMicrophone}
+        >
+          {micOn ? <MicIcon /> : <MicOffIcon />}
+        </IconButton>
+        <IconButton
+          color={cameraOn ? "active" : "inactive"}
+          onClick={toggleCamera}
+        >
+          {cameraOn ? <VideocamOutlinedIcon /> : <VideocamOffOutlinedIcon />}
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
