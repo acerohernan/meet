d722f785c4eaaea7b51c2da755de02d92bd248d0
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-04 21:10:44 -0500

    feat(web): IconButton variants added to MUI theme

--- apps/web/src/theme.ts
+++ apps/web/src/theme.ts
@@ -1,9 +1,47 @@
 import { createTheme } from "@mui/material";
 
+declare module "@mui/material/IconButton" {
+  interface IconButtonPropsColorOverrides {
+    active: true;
+    inactive: true;
+  }
+}
+
 export const theme = createTheme({
+  components: {
+    MuiIconButton: {
+      variants: [
+        {
+          props: { color: "active" },
+          style: {
+            border: "1px solid white",
+            backgroundColor: "transparent",
+            padding: "14px",
+            "&:hover": { backgroundColor: "#9A9A9C" },
+            color: "white",
+          },
+        },
+        {
+          props: { color: "inactive" },
+          style: {
+            border: "1px solid #ea4335",
+            padding: "14px",
+            backgroundColor: "#ea4335",
+            color: "white",
+            "&:hover": { backgroundColor: "#C93C33" },
+          },
+        },
+      ],
+    },
+  },
   typography: {
     button: {
       textTransform: "none",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#1a73e8",
     },
   },
   breakpoints: {
