8e2f8789c9c1ee94bbb2a663c01f15b102c60fde
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-07 08:51:54 -0500

    feat(web): redux store implementation added

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,15 +1,19 @@
+import { store } from "./store/store";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 
+import { Provider } from "react-redux";
 import { router } from "./router";
 import { theme } from "./theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
