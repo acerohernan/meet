1da90d3274664116e81da578c303d4a5c78b4836
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-10 07:39:17 -0500

    feat(web): room RTC entity added

---
+++ apps/web/src/lib/room.ts
@@ -0,0 +1,35 @@
+import { Room as RoomModel } from "@/proto/room_pb";
+import { RPC } from "./rpc";
+
+export class Room {
+  private roomInfo?: RoomModel;
+  private rpc: RPC;
+
+  constructor(public id: string, private url: string, public token: string) {
+    this.rpc = new RPC(this.url, this.token);
+  }
+
+  async startConnection(url: string, token: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const query = new URLSearchParams();
+      query.append("access_token", token);
+
+      const ws = new WebSocket(`${url}/rtc?${query.toString()}`);
+
+      ws.onerror = (event) => {
+        console.error("ws error", event);
+        reject(false);
+      };
+
+      ws.onclose = (event) => {
+        console.error("ws connection closed", event);
+        reject(false);
+      };
+
+      ws.onmessage = (message) => {
+        console.log("new meesage received", message);
+        resolve(true);
+      };
+    });
+  }
+}
