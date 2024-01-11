49b087c1fc2e113dfd9bfc4fb794c729f46016a2
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-11 09:13:54 -0500

    feat(web): wait room moved to its own folder

---
+++ apps/web/src/pages/room/wait-room/controls.tsx
@@ -0,0 +1,125 @@
+import {
+  Box,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useParams } from "react-router-dom";
+import { useCallback, useMemo, useState } from "react";
+import PresentToAllOutlinedIcon from "@mui/icons-material/PresentToAllOutlined";
+
+import { rtcService } from "@/services/rtc";
+
+import { useToast } from "@/hooks/useToast";
+import { useAccessToken } from "@/hooks/useAccessToken";
+
+interface Props {
+  isLoading: boolean;
+}
+
+export const WaitRoomControls: React.FC<Props> = ({ isLoading }) => {
+  const params = useParams();
+  const roomId = useMemo(() => params.roomId ?? "", [params]);
+  const toast = useToast();
+  const [token] = useAccessToken({ roomId });
+  const [isConnecting, setIsConnecting] = useState(false);
+
+  const attempConnection = useCallback(async () => {
+    setIsConnecting(true);
+    try {
+      const success = rtcService.connectToRoom(roomId, token);
+      console.log({ success });
+    } catch (error) {
+      console.log(error);
+      toast.error("Error at connecting with server via websocket!");
+    } finally {
+      setIsConnecting(false);
+    }
+  }, [roomId, toast, token]);
+
+  if (isLoading)
+    return (
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h4" fontWeight={400} fontSize="1.8rem">
+          Getting ready...
+        </Typography>
+        <Typography marginTop={3} fontSize="0.9rem">
+          You'll be able to join in just a moment
+        </Typography>
+        <Box marginTop={2}>
+          <CircularProgress />
+        </Box>
+      </Box>
+    );
+
+  if (!token)
+    return (
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h4" fontWeight={400} fontSize="1.8rem">
+          What's your name?
+        </Typography>
+        <TextField
+          label="Name"
+          variant="filled"
+          fullWidth
+          sx={{ maxWidth: "300px", marginTop: 4 }}
+        />
+        <Box marginTop={4} display="flex" alignItems="center" gap={1}>
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: "25px",
+              padding: "10px 25px",
+            }}
+          >
+            Ask to join
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              borderRadius: "25px",
+              padding: "10px 25px",
+            }}
+            startIcon={<PresentToAllOutlinedIcon />}
+          >
+            Present
+          </Button>
+        </Box>
+      </Box>
+    );
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Typography variant="h4" fontWeight={400} fontSize="1.8rem">
+        Ready to join?
+      </Typography>
+      <Typography marginTop={3} fontSize="0.9rem">
+        No one else here
+      </Typography>
+      <Box marginTop={2} display="flex" alignItems="center" gap={1}>
+        <Button
+          variant="contained"
+          disabled={isConnecting}
+          sx={{
+            borderRadius: "25px",
+            padding: "10px 25px",
+          }}
+          onClick={attempConnection}
+        >
+          Join now
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{
+            borderRadius: "25px",
+            padding: "10px 25px",
+          }}
+          startIcon={<PresentToAllOutlinedIcon />}
+        >
+          Present
+        </Button>
+      </Box>
+    </Box>
+  );
+};
--- apps/web/src/pages/room/wait-room/index.tsx
+++ apps/web/src/pages/room/wait-room/index.tsx
@@ -1,11 +1,12 @@
+import { useMemo } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Box, Button, CircularProgress, Typography } from "@mui/material";
-import PresentToAllOutlinedIcon from "@mui/icons-material/PresentToAllOutlined";
+import { Box, Button, Typography } from "@mui/material";
 
 import { GuestPreview } from "@/components/room/guest-preview";
 
 import { useRoomVerification } from "@/hooks/useRoomVerification";
-import { useMemo } from "react";
+
+import { WaitRoomControls } from "./controls";
 
 export const WaitRoom = () => {
   const { roomId } = useParams();
@@ -70,53 +71,7 @@ export const WaitRoom = () => {
         <Box maxWidth="800px">
           <GuestPreview isLoading={isLoading} />
         </Box>
-        {isLoading ? (
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <Typography variant="h4" fontWeight={400} fontSize="1.8rem">
-              Getting ready...
-            </Typography>
-            <Typography marginTop={3} fontSize="0.9rem">
-              You'll be able to join in just a moment
-            </Typography>
-            <Box marginTop={2}>
-              <CircularProgress />
-            </Box>
-          </Box>
-        ) : (
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <Typography variant="h4" fontWeight={400} fontSize="1.8rem">
-              Ready to join?
-            </Typography>
-            <Typography marginTop={3} fontSize="0.9rem">
-              No one else here
-            </Typography>
-            <Box marginTop={2} display="flex" alignItems="center" gap={1}>
-              <Button
-                variant="contained"
-                sx={{
-                  borderRadius: "25px",
-                  padding: "10px 25px",
-                  fontWeight: 400,
-                  fontSize: "0.9rem",
-                }}
-              >
-                Join now
-              </Button>
-              <Button
-                variant="outlined"
-                sx={{
-                  borderRadius: "25px",
-                  padding: "10px 25px",
-                  fontWeight: 400,
-                  fontSize: "0.9rem",
-                }}
-                startIcon={<PresentToAllOutlinedIcon />}
-              >
-                Present
-              </Button>
-            </Box>
-          </Box>
-        )}
+        <WaitRoomControls isLoading={isLoading} />
       </Box>
     </Box>
   );
