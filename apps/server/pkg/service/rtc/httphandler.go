fc729bb4ac80b4c07daca709a93d352fa1aab665
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-14 10:03:06 -0500

    feat(server): listen to signal request and responses from rtc enpoint

--- apps/server/pkg/service/rtc/httphandler.go
+++ apps/server/pkg/service/rtc/httphandler.go
@@ -1,15 +1,19 @@
 package rtc
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/acerohernan/meet/core"
 	"github.com/acerohernan/meet/pkg/config/logger"
 	"github.com/acerohernan/meet/pkg/service/auth"
 	"github.com/gorilla/websocket"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
 	upgrader = websocket.Upgrader{
+		CheckOrigin:     func(r *http.Request) bool { return true },
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
@@ -28,30 +32,106 @@ func (m *rtcManager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//upgrade connection
+	nodeID, err := m.store.GetNodeForRoom(m.ctx, grants.RoomID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := m.StartParticipantSignal(nodeID, grants)
+	if err != nil {
+		logger.Infow("error at start participant signal", err, "grants", grants)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer func() {
+		err := m.CloseParticipantSignal(nodeID, grants)
+		if err != nil {
+			logger.Errorw("error at closing participant signal", err)
+		}
+	}()
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	defer conn.Close()
 
 	if err != nil {
-		logger.Errow("error at upgrade ws connection", err)
+		logger.Errorw("error at upgrade ws connection", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	// start participant signal
-	err = m.StartParticipantSignal(conn, grants)
+	logger.Infow("new ws connection established!", "participant", grants.ID)
+
+	defer func() {
+		logger.Infow("ws connection closed", "participant", grants.ID)
+	}()
+
+	// send join response
+	data, err := proto.Marshal(res)
 
 	if err != nil {
-		logger.Errow("couldnt start participant signal", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	// TODO:
-	// store participant in redis with node id
-	// get node for roomID
-	// send node message to start session
-	// add participant to local store
-	// write node message to node with active connection to participant (SignalRequest)
-	// read the message and send to participant (SignalResponse)
+	err = conn.WriteMessage(websocket.BinaryMessage, data)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	go func() {
+		for {
+			select {
+			case <-r.Context().Done():
+
+				return
+			case msg := <-m.GetParticipantResponses(grants.ID):
+				logger.Infow("sending response to participant", "msg", msg)
+				// send participant responses
+				data, err := proto.Marshal(msg)
+				if err != nil {
+					logger.Errorw("error at encoding signal response", err, "participant", grants.ID)
+				}
+
+				err = conn.WriteMessage(websocket.BinaryMessage, data)
+				if err != nil {
+					logger.Errorw("error at sending signal response", err, "participant", grants.ID)
+				}
+			}
+		}
+
+	}()
+
+	for {
+		messageType, payload, err := conn.ReadMessage()
+		if err != nil {
+			if websocket.IsCloseError(
+				err,
+				websocket.CloseAbnormalClosure,
+				websocket.CloseGoingAway,
+				websocket.CloseNormalClosure,
+				websocket.CloseNoStatusReceived,
+			) {
+				logger.Infow("exit ws read loop due to closed connection", "participant", grants.ID)
+			} else {
+				logger.Errorw("error reading from websocket", err)
+			}
+			return
+		}
+
+		msg := &core.SignalRequest{}
+
+		switch messageType {
+		case websocket.BinaryMessage:
+			err := proto.Unmarshal(payload, msg)
+			if err == nil {
+				logger.Infow("signal request received", "msg", msg)
+			}
+		default:
+			logger.Errorw("ws message not supported", errors.New("ws msg type not supported"), "type", messageType)
+		}
+	}
 }
