ee190ae0046127700194dd0fb31b809cc078d863
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-14 09:55:25 -0500

    feat(server): room manager start signal methods

--- apps/server/pkg/service/rtc/manager.go
+++ apps/server/pkg/service/rtc/manager.go
@@ -2,6 +2,7 @@ package rtc
 
 import (
 	"context"
+	"errors"
 	"sync"
 	"time"
 
@@ -17,25 +18,28 @@ var (
 )
 
 type rtcManager struct {
-	ctx    context.Context
-	mu     sync.Mutex
-	store  storage.ObjectStore
-	router *router.Router
+	ctx       context.Context
+	mu        sync.RWMutex
+	store     storage.ObjectStore
+	router    *router.Router
+	localNode *core.Node
+
+	// map of participantID -> SignalResponse
+	participantResponses map[string]chan *core.SignalResponse
 
 	// map of roomID -> Room
 	rooms map[string]*Room
-	// map of participantID -> Participant
-	participants map[string]*Participant
 }
 
-func NewRTCManager(store storage.ObjectStore, router *router.Router) RTCManager {
+func NewRTCManager(localNode *core.Node, store storage.ObjectStore, router *router.Router) RTCManager {
 	m := &rtcManager{
-		mu:           sync.Mutex{},
-		rooms:        make(map[string]*Room),
-		participants: make(map[string]*Participant),
-		store:        store,
-		ctx:          context.Background(),
-		router:       router,
+		mu:                   sync.RWMutex{},
+		store:                store,
+		ctx:                  context.Background(),
+		router:               router,
+		localNode:            localNode,
+		rooms:                make(map[string]*Room),
+		participantResponses: make(map[string]chan *core.SignalResponse),
 	}
 
 	m.router.OnNodeMessage(m.handleNodeMessage)
@@ -63,10 +67,105 @@ func (m *rtcManager) CreateRoom(ctx context.Context, roomID string) error {
 		return err
 	}
 
+	m.mu.RLock()
+	nodeID := m.localNode.Id
+	m.mu.RUnlock()
+
+	// store room in local node
+	if err := m.store.SetNodeForRoom(ctx, roomID, nodeID); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (m *rtcManager) StartParticipantSignal(grants *auth.Grants) error {
+func (m *rtcManager) GetRoom(_ context.Context, roomID string) (*Room, error) {
+	m.mu.RLock()
+	room := m.rooms[roomID]
+	m.mu.RUnlock()
 
-	return nil
+	if room == nil {
+		return nil, RoomNotExistsInLocalNode
+	}
+	return room, nil
+}
+
+func (m *rtcManager) StartParticipantSignal(nodeID string, grants *auth.Grants) (*core.SignalResponse, error) {
+	msg := &core.NodeMessage{
+		Message: &core.NodeMessage_SignalRequest{
+			SignalRequest: &core.SignalRequest{
+				ParticipantId: grants.ID,
+				RoomId:        grants.RoomID,
+				Request: &core.SignalRequest_StartSession{
+					StartSession: &core.StartSession{
+						Id:     grants.ID,
+						Name:   grants.Name,
+						NodeId: m.localNode.Id,
+						Permissions: &core.ParticipantPermissions{
+							RoomAdmin: grants.RoomAdmin,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// create chann for listening participant response
+	m.mu.Lock()
+	m.participantResponses[grants.ID] = make(chan *core.SignalResponse)
+	m.mu.Unlock()
+
+	if err := m.router.SendNodeMessage(nodeID, msg); err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	for {
+		select {
+		case <-ctx.Done():
+			// delete created chann
+			m.mu.Lock()
+			delete(m.participantResponses, grants.ID)
+			m.mu.Unlock()
+			return nil, errors.New("timeout at waiting for participant signal")
+		case msg := <-m.GetParticipantResponses(grants.ID):
+			if msg == nil {
+				return nil, errors.New("signal response not sended by host node")
+			}
+			return msg, nil
+		}
+	}
+}
+
+func (m *rtcManager) CloseParticipantSignal(nodeID string, grants *auth.Grants) error {
+	msg := &core.NodeMessage{
+		Message: &core.NodeMessage_SignalRequest{
+			SignalRequest: &core.SignalRequest{
+				ParticipantId: grants.ID,
+				RoomId:        grants.RoomID,
+				Request:       &core.SignalRequest_CloseSession{},
+			},
+		},
+	}
+
+	return m.router.SendNodeMessage(nodeID, msg)
+}
+
+func (m *rtcManager) ReceiveParticipantResponse(participantID string, res *core.SignalResponse) {
+	m.mu.RLock()
+	chann := m.participantResponses[res.ParticipantId]
+	m.mu.RUnlock()
+	// if channel do not exists, ignore message
+	if chann == nil {
+		return
+	}
+	chann <- res
+}
+
+func (m *rtcManager) GetParticipantResponses(participantID string) chan *core.SignalResponse {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	return m.participantResponses[participantID]
 }
