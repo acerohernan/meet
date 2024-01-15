21f163051a77865986f7d42f19d746f67a08ac83
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-15 07:37:26 -0500

    feat(web): room entity constructor updated

--- apps/web/src/lib/room.ts
+++ apps/web/src/lib/room.ts
@@ -1,35 +1,19 @@
+import { JoinResponse } from "@/proto/rtc_pb";
 import { Room as RoomModel } from "@/proto/room_pb";
+
 import { RPC } from "./rpc";
+import { SignalClient } from "./signal";
 
 export class Room {
-  private roomInfo?: RoomModel;
+  private roomInfo: RoomModel;
   private rpc: RPC;
+  private signalClient: SignalClient;
 
-  constructor(private url: string, public token: string) {
-    this.rpc = new RPC(this.url, this.token);
-  }
+  constructor(url: string, token: string, ws: WebSocket, join: JoinResponse) {
+    if (!join.room) throw Error("no room found in join response");
 
-  async startConnection(url: string, token: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const query = new URLSearchParams();
-      query.append("access_token", token);
-
-      const ws = new WebSocket(`${url}/rtc?${query.toString()}`);
-
-      ws.onerror = (event) => {
-        console.error("ws error", event);
-        reject(false);
-      };
-
-      ws.onclose = (event) => {
-        console.error("ws connection closed", event);
-        reject(false);
-      };
-
-      ws.onmessage = (message) => {
-        console.log("new meesage received", message);
-        resolve(true);
-      };
-    });
+    this.rpc = new RPC(url, token);
+    this.signalClient = new SignalClient(ws);
+    this.roomInfo = join.room;
   }
 }
