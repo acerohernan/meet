443574f03c6a907ecc10f75e3badeb6e42b14cdb
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-17 07:38:11 -0500

    feat(server): methods for reporting participant status via websockets added

--- apps/server/pkg/service/rtc/paticipant_signal.go
+++ apps/server/pkg/service/rtc/paticipant_signal.go
@@ -24,6 +24,40 @@ func (p *Participant) SendRefreshToken(token string) error {
 		}})
 }
 
+func (p *Participant) SendParticipantConnected(participant *core.Participant) error {
+	return p.writeResponse(&core.SignalResponse{
+		ParticipantId: p.ID(),
+		RoomId:        p.RoomID(),
+		Response: &core.SignalResponse_ParticipantConnected{
+			ParticipantConnected: &core.ParticipantConnected{
+				Participant: participant,
+			},
+		},
+	})
+}
+
+func (p *Participant) SendParticipantUpdated(participant *core.Participant) error {
+	return p.writeResponse(&core.SignalResponse{
+		ParticipantId: p.ID(),
+		RoomId:        p.RoomID(),
+		Response: &core.SignalResponse_ParticipantUpdated{
+			ParticipantUpdated: &core.ParticipantUpdated{
+				Participant: participant,
+			},
+		},
+	})
+}
+
+func (p *Participant) SendParticipantDisconnected(participantID string) error {
+	return p.writeResponse(&core.SignalResponse{
+		ParticipantId: p.ID(),
+		RoomId:        p.RoomID(),
+		Response: &core.SignalResponse_ParticipantDisconnected{
+			ParticipantDisconnected: &core.ParticipantDisconnected{
+				ParticipantId: participantID},
+		}})
+}
+
 func (p *Participant) writeResponse(res *core.SignalResponse) error {
 	return p.router.SendNodeMessage(p.NodeID(), &core.NodeMessage{
 		Message: &core.NodeMessage_SignalResponse{
