4fac27858f7d4e847d483320ca2ceb3850781cac
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-26 22:23:44 -0500

    feat(web): store plain participants and guests in redux

--- apps/web/src/store/room/index.ts
+++ apps/web/src/store/room/index.ts
@@ -1,15 +1,23 @@
+import { PlainMessage } from "@bufbuild/protobuf";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+import { Guest } from "@/proto/guest_pb";
+import { Participant } from "@/proto/room_pb";
 
 import { DrawerSection } from "@/context/room/types";
 
 export interface RoomState {
   drawerSection: DrawerSection;
   isDrawerOpen: boolean;
+  participants: PlainMessage<Participant>[];
+  guests: PlainMessage<Guest>[];
 }
 
 const initialState: RoomState = {
   drawerSection: DrawerSection.People,
   isDrawerOpen: false,
+  participants: [],
+  guests: [],
 };
 
 const roomSlice = createSlice({
@@ -22,6 +30,15 @@ const roomSlice = createSlice({
     },
     closeDrawer: (state) => {
       state.isDrawerOpen = false;
+    },
+    participantsReceived: (
+      state,
+      action: PayloadAction<PlainMessage<Participant>[]>
+    ) => {
+      state.participants = action.payload;
+    },
+    guestsReceived: (state, action: PayloadAction<PlainMessage<Guest>[]>) => {
+      state.guests = action.payload;
     },
   },
 });
