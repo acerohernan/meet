d437ac58fd1b9a2bae7802ca1013223b0afb444b
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-29 08:13:38 -0500

    feat(server): handler for room admin answering guest request added

--- apps/server/pkg/service/rtc/signalhandler.go
+++ apps/server/pkg/service/rtc/signalhandler.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/acerohernan/meet/core"
 	"github.com/acerohernan/meet/pkg/config/logger"
+	"github.com/acerohernan/meet/pkg/service/auth"
+	"github.com/acerohernan/meet/pkg/utils"
 )
 
 func (m *rtcManager) handleSignalRequest(room *Room, participant *Participant, req *core.SignalRequest) error {
@@ -65,6 +67,47 @@ func (m *rtcManager) handleSignalRequest(room *Room, participant *Participant, r
 	case *core.SignalRequest_CloseSession:
 		room.DeleteParticipant(req.ParticipantId)
 		room.SendParticipantDisconnected(req.ParticipantId)
+
+	case *core.SignalRequest_AnswerGuestRequest:
+		resChan := m.GetGuestResponseChan(msg.AnswerGuestRequest.GuestId)
+		if resChan == nil {
+			return nil
+		}
+		guest := room.GetGuest(msg.AnswerGuestRequest.GuestId)
+		if guest == nil {
+			return nil
+		}
+
+		res := &core.GuestJoinResponse{Guest: guest}
+
+		switch msg.AnswerGuestRequest.Answer.(type) {
+		case *core.AnswerGuestRequest_GuestDenied:
+			res.Answer = &core.GuestJoinResponse_JoinDenied{}
+
+		case *core.AnswerGuestRequest_GuestAccepted:
+			// create new participant grants
+			at, err := m.authSvc.NewAccessTokenFromGrants(&auth.Grants{
+				ID:        utils.NewId(utils.ParticipantPrefix),
+				Name:      guest.Name,
+				RoomID:    room.ID(),
+				RoomAdmin: false,
+			})
+			if err != nil {
+				return err
+			}
+
+			res.Answer = &core.GuestJoinResponse_JoinApproved{
+				JoinApproved: &core.JoinApproved{
+					AccessToken: at.ToJWT(),
+				},
+			}
+
+		default:
+			return nil
+		}
+
+		resChan <- res
+		room.DeleteGuest(msg.AnswerGuestRequest.GuestId)
 	}
 
 	return nil
