c5f0f6d2c524a587bd2bb63c14574d9fd4711bb8
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-01-29 07:59:35 -0500

    feat(web): custom hooks for participants and guest deleted for use redux instead

--- apps/web/src/context/room/hooks.ts
+++ apps/web/src/context/room/hooks.ts
@@ -1,9 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-
-import { RoomEvents } from "@/lib/events";
-
-import { Guest } from "@/proto/guest_pb";
-import { Participant } from "@/proto/room_pb";
+import { useContext } from "react";
 
 import { RoomContext } from "./index";
 
@@ -11,54 +6,4 @@ export const useRoomContext = () => {
   const ctx = useContext(RoomContext);
   if (!ctx) throw new Error("do not use room context outside the room page");
   return ctx;
-};
-
-export const useParticipants = (): Participant[] => {
-  const { room } = useRoomContext();
-
-  const [participants, setParticipants] = useState<Participant[]>([]);
-
-  useEffect(() => {
-    if (!room) return;
-
-    const handleParticipantChanges = () => {
-      setParticipants(room.participants);
-    };
-
-    room.on(RoomEvents.ParticipantConnected, handleParticipantChanges);
-    room.on(RoomEvents.ParticipantUpdated, handleParticipantChanges);
-    room.on(RoomEvents.ParticipantDisconnected, handleParticipantChanges);
-
-    return () => {
-      room.off(RoomEvents.ParticipantConnected, handleParticipantChanges);
-      room.off(RoomEvents.ParticipantUpdated, handleParticipantChanges);
-      room.off(RoomEvents.ParticipantDisconnected, handleParticipantChanges);
-    };
-  }, [room]);
-
-  return participants;
-};
-
-export const useGuests = (): Guest[] => {
-  const { room } = useRoomContext();
-
-  const [guests, setGuests] = useState<Guest[]>([]);
-
-  useEffect(() => {
-    if (!room) return;
-
-    const handleParticipantChanges = () => {
-      setGuests(room.guests);
-    };
-
-    room.on(RoomEvents.GuestRequestReceived, handleParticipantChanges);
-    room.on(RoomEvents.GuestRequestCancelled, handleParticipantChanges);
-
-    return () => {
-      room.off(RoomEvents.GuestRequestReceived, handleParticipantChanges);
-      room.off(RoomEvents.GuestRequestCancelled, handleParticipantChanges);
-    };
-  }, [room]);
-
-  return guests;
 };
